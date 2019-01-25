bc82125f326efe00acb5302945fa4a768cc262fa
Владислав Сухов <suhov.vlad-04@yandex.ru>
2019-01-26 00:31:13 +0600

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter on5xelte on5xeltejv,$(TARGET_DEVICE)),)
+ifneq ($(filter j2corelte,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
